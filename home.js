8fb1f8222483a147adc70859fe62557b84970f77
baazarova <iskandarovaogiloy@gmail.com>
2022-10-28 10:40:53 +0500

    7-dasr masalalari

---
+++ home.js
@@ -0,0 +1,28 @@
+//PROBLEM 32
+
+// let a = 12;
+// let b = 98;
+
+// function addNumber(son1, son2){
+//    console.log(son1 + son2);
+// }
+
+// addNumber(a, b)
+
+
+
+//PROBLEM 144
+
+// let a = [-1, 4, -5]
+// function plusNumber(son){
+//     for(let i = 0; i < son.length; i++){
+//         if(son[i] < 0){
+//            console.log( son[i] *= -1);
+          
+//         }
+//         else{
+//         console.log(son[i]);
+//         }
+//     }
+// }
+//  plusNumber(a)
---
+++ index.js
@@ -0,0 +1,23 @@
+//sonlarni musbat butun sonlarga o'zgartirish:
+
+// let numbers = [3.02, -3.65, 5, -9];
+
+// function normaNumber(num){
+//     for( let i = 0; i < num.length; i++){
+//         if(num[i] < 0){
+//             num[i] *= -1;
+//            console.log( Math.round(num[i]));
+//         }
+//         else{
+//             num[i]
+//             console.log( Math.round(num[i]));
+//         }
+//     }
+// }
+
+
+// let son = normaNumber(numbers);
+// console.log(son);
+
+
+
