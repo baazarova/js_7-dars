89d5c8975029c1b4d4ea7cdcb4ed238fee71e8dd
baazarova <iskandarovaogiloy@gmail.com>
2022-10-28 11:01:15 +0500

    adding

--- home.js
+++ home.js
@@ -31,13 +31,28 @@
 
 //PROBLEM 33
 
-let son = prompt('son kiriting');
-let idf = 0;
+// let son = prompt('son kiriting');
+// let idf = 0;
 
-for(let i = 2; i < son; i++){
-    if(son % i == 0){
-        idf += 1;
-    }
-}
+// for(let i = 2; i < son; i++){
+//     if(son % i == 0){
+//         idf += 1;
+//     }
+// }
 
-idf ? console.log('son tub emas') : console.log('son tub');
+// idf ? console.log('son tub emas') : console.log('son tub');
+
+
+//PROBLEM 41
+
+// let numbers = [21, 1, 3, 5];
+// let add = 5;
+// let result = 0;
+// function addNumbers (son) {
+//     for(let i = 0; i < son.length; i++){
+//        result = son[i] + add;
+//        console.log(result);
+//     }
+// }
+
+// addNumbers(numbers)
